05bb00081b24955bbaa8c6392e9b324f0ba52ad1
David Mendoza <davidmendozaofficial@gmail.com>
2021-10-27 16:55:45 +0800

    Change label to book now
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/booking/includes/toolbar.blade.php
+++ resources/views/booking/includes/toolbar.blade.php
@@ -2,7 +2,7 @@
 	<a href="{{ route('transactions.index') }}" class="btn btn-info" data-toggle="tooltip" title="View All">
 		<span class="fa fa-list"></span>
 	</a>
-	<a href="{{ route('book-now') }}" class="btn btn-primary" data-toggle="tooltip" title="Add Transaction">
+	<a href="{{ route('book-now') }}" class="btn btn-primary" data-toggle="tooltip" title="Book Now">
 		<span class="fa fa-plus"></span>
 	</a>
 </div>
