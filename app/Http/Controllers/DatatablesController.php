b68bbea13ae789e3cdde627eeca7a96090cd73a5
David Mendoza <davidmendozaofficial@gmail.com>
2021-11-10 22:40:31 +0800

    fix2
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DatatablesController.php
+++ app/Http/Controllers/DatatablesController.php
@@ -24,9 +24,19 @@ class DatatablesController extends Controller
 	public function transactions(){
 		if(auth()->user()->role == "Admin"){
 			$transactions = Transactions::all();
+			$array = [];
+
 			foreach($transactions as $transaction){
 				$transaction->actions = "";
+
+				$temp = User::where('id', $transaction->tid)->select('fname as rfname', 'lname as rlname', 'contact as rcontact')->first();
+				$transaction = array_merge($transaction->toArray(), $temp != null ? $temp->toArray() : [
+				    "rfname" => null,
+				    "rlname" => null,
+				    "rcontact" => null
+				]);
 			}
+			$transactions = $array;
 		}
 		else if(auth()->user()->role == "Seller"){
 			$transactions = Transactions::where('sid', auth()->user()->id)
@@ -53,9 +63,18 @@ class DatatablesController extends Controller
 		}
 		else{
 			$transactions = Transactions::where('tid', auth()->user()->id)->get();
+			$array = [];
 			foreach($transactions as $transaction){
 				$transaction->actions = $transaction->actions;
+
+				$temp = User::where('id', $transaction->tid)->select('fname as rfname', 'lname as rlname', 'contact as rcontact')->first();
+				$transaction = array_merge($transaction->toArray(), $temp != null ? $temp->toArray() : [
+				    "rfname" => null,
+				    "rlname" => null,
+				    "rcontact" => null
+				]);
 			}
+			$transactions = $array;
 		}
 		// ADD USER ATTRIBUTES MANUALLY TO BE SEEN IN THE JSON RESPONSE
 
